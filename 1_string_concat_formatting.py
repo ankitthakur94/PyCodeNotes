9b9d7a92d26b41a11bfe4bf53653bb096c8cb543
Ankit Thakur <ankitthakur94@gmail.com>
2017-11-18 16:22:40 +0530

    Updating String formatting

--- 1_string_concat_formatting.py
+++ 1_string_concat_formatting.py
@@ -1,3 +1,24 @@
+#### Objective
+# 1. Explore various ways to concat strings
+#	1.A) Using + 
+#		- concat_str = string1 + string2 + string 3
+#		- Most readable, Less efficient, Creates copies, Not-scalable, Not-Recommended
+#
+#	1.B) Using ,
+#		print ( string1, string2, string3)
+#		- Can be used in print
+#		- Can use it casually
+#		- Quite readable.
+#
+#	2.C) Using Join
+#		'<string_to_join_with>.join( [ string1, string2, string3] )
+#		- Quite efficient, Scalable, Recommended (specially to concat 2/more stringso)
+#		- Join can be used to join any iterable with a string b.w each element.
+#
+# 2. String formatting
+#	- Recommended way to string formatting is to use "<string>".format(<arguments>)
+################	
+
 
 names = ['Ankit', 'Karan', 'Madhur', 'Aniket' ]
 
